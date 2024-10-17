1a17416ddb37dc83321543e357f1cdb15cbf284c
Akash-Behara <behara.akash68@gmail.com>
2024-10-17 18:01:25 +0530

    update api endpoint

--- src/lib/constants.jsx
+++ src/lib/constants.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://139.59.58.53:3000"
+export const BASE_URL = "https://api.thewpl.xyz"
 
 export const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
