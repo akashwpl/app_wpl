3e59fc7798bfdd8584ddf5095f083a0e5d1936e6
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-11-29 04:42:13 +0530

    wolf tab and gigs card hover effect

--- src/components/home/Statistics.jsx
+++ src/components/home/Statistics.jsx
@@ -2,7 +2,7 @@ const Statistics = ({ userDetails }) => {
 
   return (
     <div className="mb-8">
-        <p className="font-gridular text-primaryYellow text-[20px] leading-[24px] mb-8">Hey {userDetails ? userDetails?.displayName : "User"}, Here's your Earnings!</p>
+        <p className="font-gridular text-primaryYellow text-[20px] leading-[24px] mb-8">Hey {userDetails ? userDetails?.displayName : "User"}, keep up the participation rate!</p>
         <div className="flex flex-row text-primaryBlue">
             <div className="mr-3 w-2/4 h-[101px] py-5 px-5 bg-cover bg-[url('assets/images/total_earned_bg.png')] rounded-md">
                 <p className='font-inter font-medium text-[12px] leading-[14.4px] mb-1'>Total Earned</p>
