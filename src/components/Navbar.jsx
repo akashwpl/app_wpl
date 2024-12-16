67a75f9595e882d751c6ad1e29987451be3d7c95
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-12-17 02:15:39 +0530

    Fix: Mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,7 +139,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword')  && token &&
+        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword')  && 
           <div className='flex items-center gap-4'>
             <div className='hidden md:block'>
               <Link className='relative' to='/notifications'>
@@ -281,7 +281,7 @@ const Navbar = () => {
           <Link to={'/'}><img src={wpllogo} alt='wolf logo' className='translate-x-14 w-6 h-7' /></Link>
         </div>
         
-        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword')  && token &&
+        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword')  && 
         <div className='flex items-center gap-4'>
           {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword') && token && 
             <div className='-translate-x-12'>
@@ -303,60 +303,73 @@ const Navbar = () => {
 
         {showNavbar && <div onClick={handleShowNavbar} className='absolute -top-8 left-0 h-screen w-full bg-[#16237F]/30 backdrop-blur-sm z-[50]' />}
 
-        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword') && token &&
+        {!pathname?.includes('onboarding') && !pathname?.includes('forgetpassword') && 
         <div className={`absolute -top-8 left-0 ${showNavbar ? 'translate-y-0' : '-translate-y-[900px]'} transition-all duration-500 w-full bg-[#16237F] z-50`}>
-          <div className='flex flex-col justify-center items-center text-center bg-[#16237F] font-bienvenue mt-20 text-[24px] text-primaryYellow'>
-            <Link to={'/profile'}>
-              <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                <img src={profileSVG} alt="profile" className='size-[20px]' />
-                <p>Profile</p>
-              </div>
-            </Link>
-            {userDetail?.role !== 'user' && (
-              <Link to={'/allprojects'}>
+          {
+            token ?
+            <div className='flex flex-col justify-center items-center text-center bg-[#16237F] font-bienvenue mt-20 text-[24px] text-primaryYellow'>
+              <Link to={`/profile/${userDetail?.socials?.discord}`} onClick={() => setShowNavbar(false)} >
                 <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                  <img src={docSVG} alt="projects" className='size-[20px]' />
-                  <p>Explore</p>
+                  <img src={profileSVG} alt="profile" className='size-[20px]' />
+                  <p>Profile</p>
                 </div>
               </Link>
-            )}
-            {userDetail?.role === 'admin' && (
-              <Link to={'/requests'}>
-                <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                  <img src={hourglassSVG} alt="requests" className='size-[20px]' />
-                  <p>Requests</p>
-                </div>
-              </Link>
-            )}
-            <Link to={'/'}>
-              <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                <img src={listSVG} alt="dashboard" className='size-[20px]' />
-                <p>Dashboard</p>
-              </div>
-            </Link>
-            <Link to={'/rewards'}>
-              <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                <img src={trophySVG} alt="rewards" className='size-[20px]' />
-                <p>Rewards</p>
-              </div>
-            </Link>
-            {userDetail?.role === 'user' && (
-                <Link
-                  to="/verifyorg"
-                >
+              {userDetail?.role !== 'user' && (
+                <Link to={'/allprojects'}> onClick={() => setShowNavbar(false)}
                   <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                    <img src={tickSVG} alt="Join as org icon" className='size-[20px]' />
-                    <p>Join as Org</p>
-                  </div> 
+                    <img src={docSVG} alt="projects" className='size-[20px]' />
+                    <p>Explore</p>
+                  </div>
                 </Link>
-            )}
-            <div onClick={signout} className='text-[#E38070]'>
-              <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
-                <LogOutIcon size={30} className='rotate-180'/>
-                <p>Logout</p>
+              )}
+              {userDetail?.role === 'admin' && (
+                <Link to={'/requests'}> onClick={() => setShowNavbar(false)}
+                  <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
+                    <img src={hourglassSVG} alt="requests" className='size-[20px]' />
+                    <p>Requests</p>
+                  </div>
+                </Link>
+              )}
+              <Link to={'/'} onClick={() => setShowNavbar(false)}> 
+                <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
+                  <img src={listSVG} alt="dashboard" className='size-[20px]' />
+                  <p>Dashboard</p>
+                </div>
+              </Link>
+              <Link to={'/rewards'} onClick={() => setShowNavbar(false)}> 
+                <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
+                  <img src={trophySVG} alt="rewards" className='size-[20px]' />
+                  <p>Rewards</p>
+                </div>
+              </Link>
+              {userDetail?.role === 'user' && (
+                  <Link
+                    to="/verifyorg"
+                    onClick={() => setShowNavbar(false)}
+                  >
+                    <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2">
+                      <img src={tickSVG} alt="Join as org icon" className='size-[20px]' />
+                      <p>Join as Org</p>
+                    </div> 
+                  </Link>
+              )}
+              <div onClick={signout} className='text-[#E38070]'>
+                <div className="flex items-center gap-2 border-b border-white/5 w-[90%] mb-2 cursor-pointer" onClick={() => setShowNavbar(false)}>
+                  <LogOutIcon size={30} className='rotate-180'/>
+                  <p>Logout</p>
+                </div>
               </div>
             </div>
-          </div>
+            :
+            <div className='flex flex-col justify-center items-center text-center bg-[#16237F] font-bienvenue mt-20 text-[24px] text-primaryYellow'>
+              <Link to={'/onboarding'} onClick={() => setShowNavbar(false)}>
+                <div className="flex items-center gap-2  w-[90%] mb-6">
+                  <img src={profileSVG} alt="login" className='size-[20px]' />
+                  <p>Login</p>
+                </div>
+              </Link>
+            </div>
+          }
         </div>}
       </div>
     </div>
