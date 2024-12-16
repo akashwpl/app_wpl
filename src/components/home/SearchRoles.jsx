be8e6a00d42c2f15003690fe1247f14b982b1346
Farhat Kadiwala <farhat.kadiwala03@gmail.com>
2024-12-16 14:38:45 +0530

    changed search states

--- src/components/home/SearchRoles.jsx
+++ src/components/home/SearchRoles.jsx
@@ -55,16 +55,22 @@ const SearchRoles = ({ tiles, handleRemoveTile, handleKeyboardEnter, searchInput
 
             {/* {tiles?.length ==  && <p className="font-gridular text-primaryYellow text-[14px] leading-[16.8px] px-5 my-4">Search for the roles</p>} */}
             <div className="flex flex-row justify-between gap-4 py-2">
-                <div className="w-full flex bg-cardBlueBg2 border border-white7 p-1 rounded-md flex-wrap gap-2">
+                <div className="w-full flex bg-cardBlueBg2 border border-white7 p-1 rounded-md flex-wrap gap-1">
                     {tiles && tiles?.map((tile, index) => (
-                        <div className="flex justify-between items-center px-2 py-2 border-transparent focus:outline-0 rounded-[6px] text-white w-fit min-w-[100px] font-gridular text-[14px] leading-[16.8px]">
+                        <div className="flex justify-between items-center px-2 py-2 border-transparent focus:outline-0 rounded-[6px] text-white w-fit min-w-[100px] font-gridular text-[14px] leading-[16.8px] bg-white/5">
                             {tile}
                             <X className='text-white48 w-6 cursor-pointer hover:text-white64 scale-105 transition duration-300' onClick={() => handleRemoveTile(tile)} size={14}/>
                         </div>
                     ))}   
                     <div className='flex items-center gap-2 w-fit px-2 h-[32px]'>
-                        <Search className='text-white32' size={16}/>
-                        <input onKeyDown={handleKeyboardEnter} value={searchInput} onChange={handleSearch}  className='bg-transparent w-full outline-none border-none text-white88 placeholder:text-[14px] placeholder:text-white32 placeholder:font-gridular' placeholder='Search for you fav Org, role...'/>
+                        {tiles?.length === 0 && <Search className='text-white32' size={16}/>}
+                        <input 
+                            onKeyDown={handleKeyboardEnter} 
+                            value={searchInput} 
+                            onChange={handleSearch}  
+                            className='bg-transparent w-full outline-none border-none text-white88 font-gridular text-[14px] placeholder:text-[14px] placeholder:text-white32 placeholder:font-gridular' 
+                            placeholder={tiles?.length === 0 ? 'Search for your fav org, role...' : ''}
+                        />
                     </div>
                 </div>
 
