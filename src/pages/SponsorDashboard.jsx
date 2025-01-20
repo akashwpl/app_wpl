5c1d27c5a78d5d0140ac62cab51f717d28c971d1
Akash-Behara <behara.akash68@gmail.com>
2025-01-20 19:56:21 +0530

    sponsor dashbord

--- src/pages/SponsorDashboard.jsx
+++ src/pages/SponsorDashboard.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Statistics from '../components/home/Statistics'
-import { getUserDetails } from '../service/api'
-import { useSelector } from 'react-redux'
-import ExploreGigs from '../components/home/ExploreGigs'
 import { useQuery } from '@tanstack/react-query'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import fireWhitePng from '../assets/images/fire-white.png'
+import SponsorTrendingBountyCard from '../components/cards/SponsorTrendingBountyCard'
+import ExploreGigs from '../components/home/ExploreGigs'
+import RecentActivityCard from '../components/home/RecentActivityCard'
 import SponsorStatistics from '../components/home/SponsorStatistics'
+import { getUserDetails } from '../service/api'
 
 const SponsorDashboard = () => {
     const { user_id } = useSelector((state) => state)
@@ -21,7 +23,31 @@ const SponsorDashboard = () => {
                 <SponsorStatistics userDetails={userDetails} />
                 <ExploreGigs />
             </div>
+
+            <div className='flex flex-col py-6 items-center border border-y-0 border-r-0 border-l border-l-primaryYellow/20 min-h-[140vh] min-w-[380px]'>
+                <div className='w-[80%] flex flex-col gap-2 h-full'>
+                    <SponsorTrendingBountyCard />
+
+                    <div className='border border-white12 border-dashed w-full my-4'></div>
+
+                    <div className='bg-[#9CD4EC1A] rounded-md w-full px-4 py-2 flex items-center gap-3'>
+                        <div className='w-[20px] h-[20px]'>
+                            <img src={fireWhitePng} alt='fire-white' className='' />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <div className='font-gridular text-[14px] leading-4 text-white'>Hey {userDetails?.displayName}!</div>
+                            <div className='font-inter text-[12px] leading-[14px] text-white48'>Devs are going crazy for you. Add more bounties <Link to={'/addProject'} className='underline'>here.</Link></div>
+                        </div>
+                    </div>
+
+                    <div className='border border-white12 border-dashed w-full my-4'></div>
+
+                    <RecentActivityCard />
+                </div>
+            </div>
         </div>
+
+          
     )
 }
 
